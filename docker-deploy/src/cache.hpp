f32d6fd4fa526d7002192ddc1eea7a8c80c27cd5
ogg4 <ogg4@duke.edu>
2022-02-17 13:22:43 -0500

    Updating cache class, only contains expiration and response fields

--- docker-deploy/src/cache.hpp
+++ docker-deploy/src/cache.hpp
@@ -10,9 +10,14 @@
 */
 class Cache {
 private:
-  int id;
   time_t expiration; //tm_wday, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_year
-  std::vector<char> response; //std::string response; instead ?
+  std::string response;
+
+  //okay to just have HashMap where key is url string, value is cache object
+  //lru cache, double linked list and hashmap
+  //node content to be response 
+  //linked list only if lru 
+  //out of memory then reboot proxy
 
   bool is_fresh() {
     time_t now;
@@ -21,18 +26,19 @@ private:
     return time_to_expiration > 0;
   }
 public:
-  Cache (int id, int max_age) : id(id), validated(true) {
-    time (&expiration); 
-    expiration = expiration + max_age;
+  Cache (int max_age) {
+    time_t now;
+    time (&now); 
+    expiration = now + max_age;
   }
 
   void print_expiration() {
     std::cout << asctime(gmtime(&expiration));
 
     if (is_fresh()) {
-      std::cout << "Response " << id << " is still valid" << std::endl;
+      std::cout << "Response is still valid" << std::endl;
     } else {
-      std::cout << "Response " << id << " is expired" << std::endl;
+      std::cout << "Response is expired" << std::endl;
     }
   }
 };
--- docker-deploy/src/test_cache.cpp
+++ docker-deploy/src/test_cache.cpp
@@ -2,10 +2,10 @@
 #include "cache.hpp"
 
 int main () {
-  Cache valid(1, 100);
+  Cache valid(100);
   valid.print_expiration();
 
-  Cache invalid(2, -40);
+  Cache invalid(-40);
   invalid.print_expiration();
 
   return EXIT_SUCCESS;
