0a36bb39600779c1913c48fe0bf72becbdb649ba
ogg4 <ogg4@duke.edu>
2022-02-17 00:03:48 -0500

    cache class

--- docker-deploy/src/cache.hpp
+++ docker-deploy/src/cache.hpp
@@ -6,17 +6,12 @@
 #include <vector>
 
 /**
-* Class to store cached responses and metadata of reponses
+* Class to store cached responses and metadata of responses
 */
 class Cache {
 private:
   int id;
-  bool validated;
-
-  //call localtime() to populate
-  //call asctime(localtime(t)) to print in required format
   time_t expiration; //tm_wday, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_year
-
   std::vector<char> response; //std::string response; instead ?
 
   bool is_fresh() {
