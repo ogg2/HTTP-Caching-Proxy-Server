28766f86aebdaf127b73764d2ce2ce9b135c1833
ogg4 <ogg4@duke.edu>
2022-02-17 18:46:38 -0500

    Adding checks for cache-control (max-stale, min-fresh)

--- docker-deploy/src/cache.hpp
+++ docker-deploy/src/cache.hpp
@@ -3,7 +3,7 @@
 
 #include <cstdlib>
 #include <time.h>
-#include <vector>
+#include <string>
 
 /**
 * Class to store cached responses and metadata of responses
@@ -26,6 +26,7 @@ private:
     return time_to_expiration > 0;
   }
 public:
+  //max-age is defined in Cache-Control Directives
   Cache (int max_age) {
     time_t now;
     time (&now); 
@@ -50,14 +51,36 @@ public:
     }
   }
 
+  //Updates expiration of cache response
   void update_expiration(int max_age) {
     time_t now;
     time (&now);
     expiration = now + max_age;
   }
 
+  //update response information, potentially combine with update_expiration
   void update_response(std::string new_response) {
     response = new_response;
+  }
+  
+  //max-stale is defined in Cache-Control Directives
+  //if request directive indicates a max-stale value, client willing to accept
+  //response that has exceeded freshness by max_stale seconds
+  bool not_too_stale(int max_stale) {
+    time_t now;
+    time (&now);
+    time_t allowable_stale = expiration + max_stale;
+    return allowable_stale > now;
+  }
+  
+  //min-fresh is defined in Cache-Control Directives
+  //if request directive indicates a max-stale value, client requires
+  //response will still be fresh for min_fresh more seconds
+  bool will_be_fresh(int min_fresh) {
+    time_t now;
+    time (&now);
+    now = now + min_fresh;
+    return expiration > now;
   }
 };
 
--- docker-deploy/src/test_cache.cpp
+++ docker-deploy/src/test_cache.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "cache.hpp"
+#include <assert.h>
 
 int main () {
   Cache valid(100);
@@ -7,6 +8,16 @@ int main () {
 
   Cache invalid(-40);
   invalid.print_expiration();
+
+  std::cout << "Stale testing" << std::endl;
+  Cache stale(-10);
+  assert (stale.not_too_stale(11));
+  assert (!stale.not_too_stale(9));
+
+  std::cout << "Min Fresh testing" << std::endl;
+  Cache min_fresh(20);
+  assert (min_fresh.will_be_fresh(19));
+  assert (!min_fresh.will_be_fresh(21));
 
   return EXIT_SUCCESS;
 }
