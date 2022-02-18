e367a9269e5ce1361af6928583c37b86946ae80b
ogg4 <ogg4@duke.edu>
2022-02-18 16:54:55 -0500

    Connecting to port 80, Server client class

--- docker-deploy/src/main.cpp
+++ docker-deploy/src/main.cpp
@@ -3,9 +3,9 @@
 #include <sstream>
 #include <mutex>
 #include <iostream>
-#include <stdio>
+#include <cstdio>
 
-#include "server.hpp"
+#include "serverClient.hpp"
 
 #define LOGGING "Start Logging my task = %d\n \tThread ID: %s\n"
 #define THREADLOG "\tThread ID = %s\n"
@@ -13,14 +13,9 @@
 /* 
 - Socket daemon module to wait and recieve connections from clients
   - Each connection spawns a new thread to run proxy server module
-
-
-
 */
 
-//std::mutex test_mutex;
-
-char * make_GET_request(char * path, char * hostaddr) {
+/*char * make_GET_request(char * path, char * hostaddr) {
   char p_request[100]={0};
  
   char p_resourcepath[]="2019/07/creating-xml-request-in-cpp-for-server.html";
@@ -46,27 +41,27 @@ int make_GET_socket(const char * hostname) {
 
   status = getaddrinfo(hostname, port, &host_info, &host_info_list);
   if (status != 0) {
-    cerr << "Error: cannot get address info for host" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
+    std::cerr << "Error: cannot get address info for host" << std::endl;
+    std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   socket_fd = socket(host_info_list->ai_family, 
 		     host_info_list->ai_socktype, 
 		     host_info_list->ai_protocol);
   if (socket_fd == -1) {
-    cerr << "Error: cannot create socket" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
+    std::cerr << "Error: cannot create socket" << std::endl;
+    std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+    return EXIT_FAILURE;
+  }
   
   
   status = connect(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
   if (status == -1) {
-    cerr << "Error: cannot connect to socket" << endl;
-    cerr << "  (" << hostname << "," << port << ")" << endl;
-    return -1;
-  } //if
+    std::cerr << "Error: cannot connect to socket" << std::endl;
+    std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   char * resource = "resource";
 
@@ -77,16 +72,13 @@ int make_GET_socket(const char * hostname) {
   freeaddrinfo(host_info_list);
   close(socket_fd);
 
-  return 0;
-
+  return EXIT_SUCCESS;
 }
 
 void recieve_requests(Server& server, int client_id) {
 
   ssize_t len = 1;
   char buffer [512];
-  //std::lock_guard<std::mutex> guard(test_mutex);
-
 
   while (len != 0) {
     len = recv(client_id, buffer, 512, 0);
@@ -102,29 +94,29 @@ void recieve_requests(Server& server, int client_id) {
   std::cout << "X: " << x << std::endl;
   sleep(10);
   
+}*/
+
+void server_message(ServerClient server, int client_id) {
+  server.receive_message(client_id);
 }
 
-
 int main () { 
-  Server s;
-  int ret = s.server_init();
-  if (ret == EXIT_FAILURE) {
-    std::cout << "Failed to initialize server." << std::endl;
+  ServerClient server("0.0.0.0", "12345");
+  bool is_server = true;
+  int status = server.initialize_socket(is_server);
+  if (status == EXIT_FAILURE) {
+    std::cout << "Failed to setup server." << std::endl;
     exit(EXIT_FAILURE);
   }
-  int count = 0;
   while (1) {
-    sleep(2);
-    int client_id = s.accept_connections();
+    int client_id = server.accept_connections();
     if (client_id == -1) {
       std::cerr << "Error: cannot accept connection on socket" << std::endl;
     } else {
-      std::thread (count_up, std::ref(s), client_id).detach();
-      //syslog(LOG_INFO, LOGGING, count++, oss.str().c_str());
+      std::thread (server_message, std::ref(server), client_id).detach(); //do we need to use std::ref?
     }
     //process request
     //send response
   }
-  //  closelog();
   return EXIT_SUCCESS;
 }
---
+++ docker-deploy/src/serverClient.hpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <vector>
+#include <string>
+
+class ServerClient {
+private:
+  int socket_fd;
+  struct addrinfo host_info;
+  struct addrinfo * host_info_list;
+  const char * hostname;
+  const char * port;
+
+public:
+  ServerClient(const char * hostname_, const char * port_) : hostname(hostname_), port(port_) {
+    memset(&host_info, 0, sizeof(host_info));
+
+    host_info.ai_family   = AF_UNSPEC;
+    host_info.ai_socktype = SOCK_STREAM;
+    host_info.ai_flags    = AI_PASSIVE;
+  }
+
+  int initialize_socket(bool is_server) {
+    if (is_server) {
+      if (get_address_info() && create_socket() && bind_socket() && listen_socket()) {
+        std::cout << "Ready for connection on port " << port << std::endl;
+        return EXIT_SUCCESS;
+      }
+    } else {
+      if (get_address_info() && create_socket() && connect_socket()) {
+        std::cout << "Connected to " << hostname << " on port " << port << std::endl;
+        return EXIT_SUCCESS;
+      }
+    }
+    return EXIT_FAILURE;
+  }
+
+  bool get_address_info() {
+    int status = getaddrinfo(hostname, port, &host_info, &host_info_list);
+    if (status != 0) {
+      std::cerr << "Error: cannot get address info for host" << std::endl;
+      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  bool create_socket() {
+    socket_fd = socket(host_info_list->ai_family, host_info_list->ai_socktype, host_info_list->ai_protocol);
+    if (socket_fd == -1) {
+      std::cerr << "Error: cannot create socket" << std::endl;
+      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  bool bind_socket() {
+    int yes = 1;
+    int status = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    status = bind(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
+    if (status == -1) {
+      std::cerr << "Error: cannot bind socket" << std::endl;
+      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  bool listen_socket() {
+    int status = listen(socket_fd, 100);
+    if (status == -1) {
+      std::cerr << "Error: cannot listen on socket" << std::endl; 
+      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  bool connect_socket() {
+    int status = connect(socket_fd, host_info_list->ai_addr, host_info_list->ai_addrlen);
+    if (status == -1) {
+      std::cerr << "Error: cannot connect to socket" << std::endl;
+      std::cerr << "  (" << hostname << "," << port << ")" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  int accept_connections() {
+    struct sockaddr_storage socket_addr;
+    socklen_t socket_addr_len = sizeof(socket_addr);
+    int client_connection_fd = accept(socket_fd, (struct sockaddr *)&socket_addr, &socket_addr_len);
+    if (client_connection_fd == -1) {
+      std::cerr << "Error: cannot accept connection on socket" << std::endl;
+      return EXIT_FAILURE;
+    }
+    return client_connection_fd;
+  }
+
+  //recv message
+  //Response r = parse_response() return this object
+  //recv while loop to get entire response 
+
+  // append_body() text in Response class - Adam
+  // check_chunked_encoding()
+  // content_length()
+
+  //make_response() and forward to client socket 0.0.0.0 12345
+  //close
+  int receive_message(int client_connection_fd) {
+    char buffer[512];
+    recv(client_connection_fd, buffer, 512, 0);
+    buffer[512] = 0;
+
+    //Response response = parse_response(buffer);
+
+    std::ofstream myfile;
+    myfile.open ("log.txt");
+    
+    myfile << "Server received: " << buffer << std::endl;
+    std::cout << "Server received: " << buffer << std::endl;
+    myfile.close();
+    return EXIT_SUCCESS;
+  }
+
+  int client_receive() {
+    return receive_message(socket_fd);
+  }
+
+  bool send_message(std::vector<char> message) {
+    std::string buffer_string = std::string(message.begin(), message.end()); 
+    const char * buffer = buffer_string.c_str();
+
+    ssize_t status = send(socket_fd, buffer, message.size(), 0);    
+    if (status == -1) {
+      std::cerr << "Error: could not send message on socket" << std::endl;
+      return false;
+    }
+    return true;
+  }
+
+  void close_socket() {
+    freeaddrinfo(host_info_list);
+    close(socket_fd);
+  }
+};
