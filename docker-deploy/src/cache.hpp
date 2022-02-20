ae7d3850bb131df8211dfe955154705ec125d932
ogg4 <ogg4@duke.edu>
2022-02-20 16:12:48 -0500

    Fixing cache compile errors

--- docker-deploy/src/cache.hpp
+++ docker-deploy/src/cache.hpp
@@ -6,24 +6,22 @@
 
 class Cache {
 private:
-  std::unordered_map<std::string, CacheEntry> cache;
+  std::unordered_map<std::string, CacheEntry *> cache;
 
 public:
-  Cache() {}
-
-  CacheEntry find_response(std::string url) {
-    std::unordered_map<std::string, CacheEntry>::const_iterator it = cache.find(url);
-    if (*it == std::unordered_map::end) {
-      return NULL;
+  CacheEntry * findResponse(std::string url) {
+    std::unordered_map<std::string, CacheEntry *>::const_iterator it = cache.find(url);
+    if (it == cache.end()) {
+      return nullptr;
     }
     return it->second;
   }
 
-  void addEntry(std::string url, CacheEntry entry) {
+  void addEntry(std::string url, CacheEntry * entry) {
     if (cache.count(url) == 0) {
       cache.emplace(url, entry); 
     } else {
-      std::unordered_map<std::string, CacheEntry>::const_iterator it = cache.find(url);
+      std::unordered_map<std::string, CacheEntry *>::iterator it = cache.find(url);
       it->second = entry;
     }
   }
