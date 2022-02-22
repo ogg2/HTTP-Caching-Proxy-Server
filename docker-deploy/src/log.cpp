f0efcbdbc2463fdf2f6556d2482ad1c70dc1dfcc
adamkrekorian <adam.krekorian@gmail.com>
2022-02-22 10:27:11 -0500

    modified docker-compose for logging

--- docker-deploy/src/log.cpp
+++ docker-deploy/src/log.cpp
@@ -8,7 +8,7 @@ void write_log(int fd, string phrase) {
   time_t cur_time = time(nullptr);
   
   std::ofstream outfile;
-  outfile.open("log.txt", std::ios_base::app);
+  outfile.open("/var/log/erss/log.txt", std::ios_base::app);
   outfile << "[" << fd << "]: " << phrase;
   outfile << " at " << asctime(localtime(&cur_time));
 }
