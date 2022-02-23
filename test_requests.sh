8fc1f490239ab4807cbc6f32c33ddae2b2be471a
adamkrekorian <adam.krekorian@gmail.com>
2022-02-23 15:28:52 -0500

    added post request to test_requests.sh

--- test_requests.sh
+++ test_requests.sh
@@ -12,8 +12,9 @@ echo "Max-Age GET Request"
 curl http://www.artsci.utoronto.ca/futurestudents -x 0.0.0.0:12345 -o test-responses/max-age-response.txt
 curl http://www.artsci.utoronto.ca/futurestudents -x 0.0.0.0:12345 -o test-responses/max-age-response.txt
 
-#echo "POST Request"
-#curl -d "custemail=a@a.com&custname=a&size=medium&topping=cheese" -X POST http://httpbin.org/forms/post -o test-responses/post-response.txt
+echo "POST Request"
+post_content="custname=a&custtel=123&custemail=a%40a.com&size=medium&topping=cheese&delivery=18%3A00&comments=none"
+curl -d $(post_content) -X POST http://httpbin.org/forms/post -o test-responses/post-response.txt
 
 echo "CONNECT Request"
 curl https://www.google.com/ -x 0.0.0.0:12345 -o test-responses/connect-response.txt
