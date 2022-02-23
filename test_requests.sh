444e821c0284ab664cc009447078c2af31d99508
adamkrekorian <adam.krekorian@gmail.com>
2022-02-22 23:55:46 -0500

    added test cases

---
+++ test_requests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p test-responses
+
+echo "GET Request"
+curl http://www.acme.com/ -x 0.0.0.0:12345 -o test-responses/get-response.txt
+
+echo "Chunked GET Request"
+curl http://www.httpwatch.com/httpgallery/chunked/chunkedimage.aspx -x 0.0.0.0:12345 -o test-responses/chunked-response.txt
+
+echo "Max-Age GET Request"
+curl http://www.artsci.utoronto.ca/futurestudents -x 0.0.0.0:12345 -o test-responses/max-age-response.txt
+curl http://www.artsci.utoronto.ca/futurestudents -x 0.0.0.0:12345 -o test-responses/max-age-response.txt
+
+#echo "POST Request"
+#curl -d "custemail=a@a.com&custname=a&size=medium&topping=cheese" -X POST http://httpbin.org/forms/post -o test-responses/post-response.txt
+
+echo "CONNECT Request"
+curl https://www.google.com/ -x 0.0.0.0:12345 -o test-responses/connect-response.txt
